4b6fb2faac900910440d69aa84c27dd6ca54fd96
Keir Badger <keir.badger@acuris.com>
2018-05-03 11:06:27 +0100

    Allow module to pass in assume_role_policy
    If passed in it will override the default one

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role" "task_role" {
       var.family :
       format("%.24stf%.4s", var.family, sha1(var.family))
   }"
-  assume_role_policy = "${data.aws_iam_policy_document.instance-assume-role-policy.json}"
+  assume_role_policy = "${var.assume_role_policy == "" ? data.aws_iam_policy_document.instance-assume-role-policy.json : var.assume_role_policy}"
 }
 
 data "aws_iam_policy_document" "instance-assume-role-policy" {
--- test/infra/main.tf
+++ test/infra/main.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+  version = "~> 1.16"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_get_ec2_platforms      = true
@@ -19,6 +20,12 @@ variable "task_volume_param" {
 variable "family_param" {
   description = "The test can set this var to be passed to the module"
   default     = "tf_ecs_task_def_test_family"
+}
+
+variable "assume_role_policy" {
+  description = "A valid IAM policy for assuming roles - optional"
+  type        = "string"
+  default     = ""
 }
 
 module "taskdef_with_role" {
@@ -49,6 +56,65 @@ END
   }
 }
 END
+  volume                = "${var.task_volume_param}"
+}
 
-  volume = "${var.task_volume_param}"
+module "taskdef_with_role_and_assume_role" {
+  source = "../.."
+
+  family = "${var.family_param}"
+
+  container_definitions = [
+    <<END
+{
+  "name": "web",
+  "image": "hello-world:latest",
+  "cpu": 10,
+  "memory": 128,
+  "essential": true
+}
+END
+    ,
+  ]
+
+  policy = <<END
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Action": "s3:ListBucket",
+    "Resource": "arn:aws:s3:::example_bucket"
+  }
+}
+END
+  volume                = "${var.task_volume_param}"
+  assume_role_policy    = "${data.aws_iam_policy_document.assume-role-policy.json}"
+}
+
+data "aws_iam_policy_document" "assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = [
+        "ecs-tasks.amazonaws.com",
+        "ec2.amazonaws.com",
+        "ecs.amazonaws.com",
+        "autoscaling.amazonaws.com",
+      ]
+    }
+  }
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::371640587010:role/autoscaler",
+        "arn:aws:iam::733578946173:role/autoscaler",
+      ]
+    }
+  }
 }
