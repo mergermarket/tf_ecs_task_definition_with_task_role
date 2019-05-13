a88e701f3ad8cd81034b79d5f696cf15145480fe
Keir Badger <keir.badger@acuris.com>
2019-05-13 16:40:32 +0100

    Add outputs for the task execution role name and arn

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "task_role_arn" {
 output "task_role_name" {
   value = "${aws_iam_role.task_role.name}"
 }
+
+output "task_execution_role_arn" {
+  value = "${aws_iam_role.ecs_tasks_execution_role.arn}"
+}
+
+output "task_execution_role_name" {
+  value = "${aws_iam_role.ecs_tasks_execution_role.name}"
+}
